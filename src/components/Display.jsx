c65a9b326fc7a4babd3e9f0ebbdc30f96a5286d3
dani <danifromecuador@gmail.com>
2024-03-12 18:57:56 -0500

    Avoid to display too big numbers

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,7 +1,17 @@
+import { useState, useEffect } from 'react'
 import '../styles/Display.css'
 
+
 export const Display = () => {
-  const result = 123456721
+  const [result, setResult] = useState(0);
+   useEffect(() => {
+    setResult(9999);
+    if (result > 999) {
+      setResult("too big")
+    }
+  }, []);
+
+  
   return (
     <div className="display">
       {result}
